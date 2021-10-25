c1dc6867b7565065bc59de2cc5700f4e58f6adf2
Cam Webb <cw@camwebb.info>
2021-10-24 22:09:18 -0800

    before tracking down polytomy #fail

--- phylomatic.R
+++ phylomatic.R
@@ -1,78 +1,98 @@
+library(ape)
 
 phylomatic <- function(phylo, taxa) {
+
+    ## node.label?
+    if(length(phylo$node.label) == 0) {
+        phylo$node.label <- vector("character", length(phylo$edge[,1]))
+        phylo$node.label[] <- ""
+        NL <- 0
+    } else {
+        NL <- 1
+    }
 
     ## edges?
     if(length(phylo$edge.length) == 0) {
         phylo$edge.length <- vector("double", length(phylo$edge[,1]))
-        ## phylo$edge.length[] <- NA
-        ## not needed, C deals with NA as NaN anyway 
+        phylo$edge.length[] <- NaN
+        ## missing BLs in existing $edge.length are NaN
+        ## NA could be used, but C deals with NA as NaN anyway
+        BL <- 0
+    } else {
+        BL <- 1
     }
-    ## recode NaNs to NAs
-    ## phylo$edge.length[is.nan(phylo$edge.length)] <- NA
+    
     ## root edge?
     if(length(phylo$root.edge) == 0) {
         phylo$root.edge <- vector("double", 1)
-        ## phylo$root.edge[] <- NA
+        phylo$root.edge[] <- NaN
+        rootBL <- 0
+    } else {
+        rootBL <- 1
     }
-    ## ## number of nodes for FY
-    ## fn     <- length(phylo$edge[,1]) + 1
 
     dyn.load("phylomatic.so")
 
-    ## TODO : null bls passed into C as 0.0 - need to inform if no bLs
-    
-    out <- .C("phylomatic",
+    x <- .C("phylomatic",
        NAOK = TRUE,
        ## INPUT (APE tree)
-       ## 1. dimension of $edge
-       as.integer(length(phylo$edge[,1])),
-       ## 2,3. vectors of edge lookup table: to, from
-       as.integer(phylo$edge[,1]),
-       as.integer(phylo$edge[,2]),
-       ## 4. branch lengths
-       as.double(phylo$edge.length),
-       ## 5. root edge
-       as.double(phylo$root.edge),
-       ## 6. number of tips 
-       as.integer(length(phylo$tip.label)),
-       ## 7. tip labels
-       as.character(phylo$tip.label),
-       ## 8. inner node labels
-       as.character(phylo$node.label),
-       ## 9. max label string length
-       as.integer(max(nchar(c(phylo$tip.label,phylo$node.label)))),
+       ## dimension of $edge
+       ape_nedge    = as.integer(length(phylo$edge[,1])),
+       ## vectors of edge lookup table: to, from
+       ape_to       = as.integer(phylo$edge[,1]),
+       ape_from     = as.integer(phylo$edge[,2]),
+       ## branch lengths
+       ape_bl       = as.double(phylo$edge.length),
+       ## root edge
+       ape_rbl      = as.double(phylo$root.edge),
+       ## number of tips 
+       ape_ntip     = as.integer(length(phylo$tip.label)),
+       ## tip labels
+       ape_tip_lab  = as.character(phylo$tip.label),
+       ## inner node labels
+       ape_node_lab = as.character(phylo$node.label),
+       ## max label string length
+       ape_max_lab_len = 
+           as.integer(max(nchar(c(phylo$tip.label,phylo$node.label)))),
 
        ## OUTPUT (APE tree)
        ## Dimension arrays as size of input megatree
-       ## 10. $Nnode
-       as.integer(vector("integer", 1)),
-       ## 11, 12. Two vectors of $edge lookup table: [,1], [,2]
-       as.integer(vector("integer", length(phylo$edge[,1]))),
-       as.integer(vector("integer", length(phylo$edge[,1]))),
-       ## 13. Branch lengths: $edge.length
-       as.double(vector("double", length(phylo$edge[,1]))),
-       ## 14. Root branch length: $root.edge
-       as.double(vector("double", 1)),
-       ## 15. $tip.label
-       as.character(vector("character", length(phylo$edge[,1]))),
-       ## 16. $node.label
-       as.character(vector("character", length(phylo$edge[,1])))
+       ## $Nnode
+       out_nnode    = as.integer(vector("integer", 1)),
+       ## Two vectors of $edge lookup table: [,1], [,2]
+       out_to       = as.integer(vector("integer", length(phylo$edge[,1]))),
+       out_from     = as.integer(vector("integer", length(phylo$edge[,1]))),
+       ## Branch lengths: $edge.length
+       out_bl       = as.double(vector("double", length(phylo$edge[,1]))),
+       ## Root branch length: $root.edge
+       out_rbl      = as.double(vector("double", 1)),
+       ## $tip.label
+       out_tip_lab  = as.character(vector("character", length(phylo$edge[,1]))),
+       ## $node.label
+       out_node_lab = as.character(vector("character", length(phylo$edge[,1]))),
+       ## 17. N tips
+       out_ntip     = as.integer(vector("integer", 1))
        )
-    
+    ## you can reference output sub-objects by out[[12]] or $out_from
     dyn.unload("phylomatic.so")
 
-    ## trim $tip.label
-    ape <- list(Nnode = out[[10]],
-                edge = cbind(out[[11]], out[[12]]),
-                tip.label = out[[15]][out[[15]] != ""],
-                node.label = out[[16]][out[[16]] != ""])
+    ## return(out)
     
-    if (length(out[[13]][!is.nan(out[[13]])]) != 0) {
-        out[[13]][is.nan(out[[13]])] <- NA
-        ape <- c(ape, list(edge.length = out[[13]]))
+    ape <- list(Nnode = x$out_nnode,
+                edge = cbind(x$out_to[  1:(x$out_nnode + x$out_ntip -1)],
+                             x$out_from[1:(x$out_nnode + x$out_ntip -1)]),
+                tip.label = x$out_tip_lab[1:x$out_ntip])
+
+    if (NL == 1) {
+        ape <- c(ape, list(node.label = x$out_node_lab[1:x$out_nnode]))
     }
-    if (!is.nan(out[[14]])) {
-        ape <- c(ape, list(root.edge = out[[14]]))
+    
+    if (BL == 1) {
+        ## x$out_bl[is.nan(x$out_bl)] <- NA
+        ape <- c(ape, list(edge.length = x$out_bl))
+    }
+    if (rootBL == 1) {
+        ape <- c(ape, list(root.edge = x$out_rbl))
     }
 
     class(ape) <- "phylo"
--- phylomatic.c
+++ phylomatic.c
@@ -1,15 +1,16 @@
 #include <R.h>
-#include <stdio.h>  // for NULL
 #include <string.h> // for strcpy
 
+// TODO: correctly handle NaN and NULL in R and C.  stdio.h for NULL?
+
 void phylomatic(int *ape_nedge,  int *ape_to,  int *ape_from,           \
-                double *ape_length,  double *ape_rlength,               \
+                double *ape_bl,  double *ape_rbl,                       \
                 int *ape_ntip,  char **ape_tip_lab,                     \
                 char **ape_node_lab, int *ape_max_lab_len,              \
                                                                         \
                 int *out_nnode, int *out_to,  int *out_from,            \
-                double *out_length,  double *out_rlength,               \
-                char **out_tip_lab, char **out_node_lab)
+                double *out_bl,  double *out_rbl,                       \
+                char **out_tip_lab, char **out_node_lab, int *out_ntip)
 {
   // NB, this APE -> FY conversion could go in the R code, but far easier
   // to use loops and arrays
@@ -19,6 +20,7 @@ void phylomatic(int *ape_nedge,  int *ape_to,  int *ape_from,           \
   int k = 1;  // node label counter (skip root)
 
   int    a2f[*ape_nedge+1];
+  int    f2a[*ape_nedge+1];
   int    fy_to[*ape_nedge+1];
   int    fy_isinner[*ape_nedge+1];
   double fy_length[*ape_nedge+1];
@@ -27,67 +29,74 @@ void phylomatic(int *ape_nedge,  int *ape_to,  int *ape_from,           \
   // Start with root node
   fy_to[0] = -1; 
   fy_isinner[0] = 1;
-  fy_length[0] = *ape_rlength;
+  fy_length[0] = *ape_rbl;
   strcpy(fy_lab[0], ape_node_lab[0]);
   a2f[ape_to[0]] = 0;  // ape-to-fy lookup: edge is root
-  Rprintf("%2d %2d %2d %3.1f %8s\n", n, fy_to[n], fy_isinner[n], fy_length[n], fy_lab[n] );
+  // Rprintf("%2d %2d %2d %3.1f %8s\n",                             \
+  //         n, fy_to[n], fy_isinner[n], fy_length[n], fy_lab[n] );
   
   // enumerate through ape's edge list
   for(i = 0; i < *ape_nedge; i++) {
     n++;
     // is it a tip node? is the edge[,1] value <= number of tips?
     if (ape_from[i] <= *ape_ntip) {
-      // Rprintf("terminal\n");
       fy_isinner[n] = 0;
       strcpy(fy_lab[n], ape_tip_lab[j]);
       j++;
     }
     else {
-      // Rprintf("inner\n");
       fy_isinner[n] = 1;
       strcpy(fy_lab[n], ape_node_lab[k]);
       k++;
     }
     // copy $edge and branch lengths
     a2f[ape_from[i]] = n;
-    fy_to[n]   = a2f[ape_to[i]];  // will always have been filled in prior loop 
-    fy_length[n]  = ape_length[i];
+    fy_to[n]   = a2f[ape_to[i]];  // will always have been filled in prior loop
+    fy_length[n]  = ape_bl[i];
     
-    Rprintf("%2d %2d %2d %3.1f %8s\n", n, fy_to[n], fy_isinner[n], fy_length[n], fy_lab[n] );
+    // Rprintf("%2d %2d %2d %3.1f %8s\n", \
+    //   n, fy_to[n], fy_isinner[n], fy_length[n], fy_lab[n] );
   }
 
-  // **fy2new**
-  *out_nnode = *ape_nedge ;
-  *out_rlength = fy_length[0] ;
+  // ----- fy2ape ----------------------------------------------------------
+  // root info:
+  *out_rbl = fy_length[0] ;
   strcpy(out_node_lab[0], fy_lab[0]);
-
+  f2a[0] = *ape_ntip + 1;
+  
   int ot = 0;  // out tip counter
   int on = 0;  // out node counter
+
   // for each member of fy, excluding the first
   for(i = 1; i < *ape_nedge+1; i++) {
     // tips
     if(fy_isinner[i] != 1) {
       ot++;
       out_from[i-1] = ot;
+      f2a[i] = ot;
       strcpy(out_tip_lab[ot-1], fy_lab[i]);
     }
     else {
       on++;
-      out_from[i-1] = i +1 + *ape_ntip;
+      f2a[i] = on + *ape_ntip + 1;
       strcpy(out_node_lab[on], fy_lab[i]);
     }
-    out_to[i-1] = fy_to[i] +1+ *ape_ntip;
-    out_length[i-1] = fy_length[i];
-  }
+    // Rprintf("%2d %2d %3.1f\n", f2a[fy_to[i]],f2a[i], fy_length[i]);
 
+    out_from[i-1] = f2a[i];
+    out_to[i-1]   = f2a[fy_to[i]];
+    out_bl[i-1] = fy_length[i];
+  }
   
+  *out_ntip = ot;
+  *out_nnode = on+1;
 }
 
 //                                       labels            
 //   APE TREE                    APE   tip  node        FY
-//                                                
-//       root = 4              4 <- 1   A             1 ->  4 A   
-//     /\ inner = 5            4 <- 5      inner      5 ->  4 inner
-//    / /\                     5 <- 2   B             2 ->  5 B
-//   A  B C                    5 <- 3   C             3 ->  5 C
-//   1  2 3                                           4 -> -1 root
+//                                                    0 -> -1 root
+//       root = 4              4 <- 1   A             1 ->  0 A   
+//     /\ inner = 5            4 <- 5      inner      2 ->  0 inner
+//    / /\                     5 <- 2   B             3 ->  2 B
+//   A  B C                    5 <- 3   C             4 ->  2 C
+//   1  2 3                                      
